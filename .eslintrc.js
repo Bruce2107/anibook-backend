334b417bdf626cebef4efd1f754e9a984142b706
Eduardo Henrique <eduardo.henrique@zup.com.br>
2020-05-13 19:11:34 -0300

    projeto criado

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    node: true,
+    es6: true,
+  },
+  extends: ['standard'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
+};
