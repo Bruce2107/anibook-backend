01dfb6ce0098c4bde62819e6fe465604981307f2
Eduardo Henrique <eduardo.henrique@zup.com.br>
2020-05-23 18:30:15 -0300

    env variables

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,9 +1,12 @@
 import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const pool = new Pool({
-  user: 'eduhenriquezup',
-  host: 'localhost',
-  password: 'anibook',
-  database: 'anibook',
-  port: 5432,
+  user: process.env.DB_USER as string | 'eduhenriquezup',
+  host: process.env.DB_HOST as string | 'localhost',
+  password: process.env.DB_PASS as string | 'anibook',
+  database: process.env.DB_NAME as string | 'anibook',
+  port: Number(process.env.DB_PORT) | 5432,
 });
