3e50baba35966c7a776c63a09b4aaa50911f8427
Eduardo Henrique <eduardo.henrique@zup.com.br>
2020-06-10 09:16:44 -0300

    manga usecase

---
+++ src/adapter/manga/repository/InMemoryManga.test.ts
@@ -0,0 +1,171 @@
+import { isManga } from 'anibook';
+import InMemoryManga from './InMemoryManga';
+import Manga from '../../../domain/manga';
+
+describe('InMemoryManga', () => {
+  let mangas: Manga[] = [];
+  let inMemoryManga: InMemoryManga;
+  beforeEach(() => {
+    mangas = [];
+    mangas.push(
+      {
+        name: 'manga1',
+        photo: 'photo.webp',
+        synopsis: 'text',
+        comment: 'text',
+        info: {
+          author: 'author',
+          status: 'status',
+          numberChapters: 12,
+          numberVolumes: 1,
+        },
+        whereRead: [{ language: 'sda', name: 'name', url: 'url' }],
+      },
+      {
+        name: 'manga2',
+        photo: 'photo.webp',
+        synopsis: 'text',
+        comment: 'text',
+        info: {
+          author: 'author',
+          status: 'status',
+          numberChapters: 12,
+          numberVolumes: 1,
+        },
+        whereRead: [{ language: 'sda', name: 'name', url: 'url' }],
+      },
+      {
+        name: 'manga3',
+        photo: 'photo.webp',
+        synopsis: 'text',
+        comment: 'text',
+        info: {
+          author: 'author',
+          status: 'status',
+          numberChapters: 12,
+          numberVolumes: 1,
+        },
+        whereRead: [{ language: 'sda', name: 'name', url: 'url' }],
+      }
+    );
+    inMemoryManga = new InMemoryManga(mangas);
+  });
+  test('should delete an manga', async () => {
+    const result = await inMemoryManga._delete('', 'manga2');
+    expect(result).toBeTruthy();
+  });
+  test('shound not delete an manga', async () => {
+    const result = await inMemoryManga._delete('', 'manga0');
+    expect(result).toBeFalsy();
+  });
+  test('should find an manga', async () => {
+    const result = await inMemoryManga.alreadyExists('', 'manga2');
+    expect(result).toBeTruthy();
+  });
+  test('should not find an manga', async () => {
+    const result = await inMemoryManga.alreadyExists('', 'manga');
+    expect(result).toBeFalsy();
+  });
+  test('should get an manga', async () => {
+    const result = await inMemoryManga.getOne('', 'manga1', []);
+    expect(isManga(result)).toBeTruthy();
+  });
+  test('should not get an manga', async () => {
+    const result = await inMemoryManga.getOne('', 'manga', []);
+    expect(result).toBeFalsy();
+  });
+  test('should return 2 mangas', async () => {
+    const result = await inMemoryManga.getRandom('', 2, []);
+    expect(result.length).toBe(2);
+  });
+  test('should return a empty array when limit is 0', async () => {
+    const result = await inMemoryManga.getRandom('', 0, []);
+    expect(result.length).toBe(0);
+  });
+  test('should insert a new manga', async () => {
+    const newManga: Manga = {
+      name: 'manga',
+      photo: 'photo.webp',
+      synopsis: 'text',
+      comment: 'text',
+      info: {
+        author: 'author',
+        status: 'status',
+        numberChapters: 12,
+        numberVolumes: 1,
+      },
+      whereRead: [{ language: 'sda', name: 'name', url: 'url' }],
+    };
+    const result = await inMemoryManga.insert('', [], newManga);
+    expect(result).toBeTruthy();
+  });
+  test('should not insert a new manga', async () => {
+    const newManga: Manga = {
+      name: 'manga2',
+      photo: 'photo.webp',
+      synopsis: 'text',
+      comment: 'text',
+      info: {
+        author: 'author',
+        status: 'status',
+        numberChapters: 12,
+        numberVolumes: 1,
+      },
+      whereRead: [{ language: 'sda', name: 'name', url: 'url' }],
+    };
+    const result = await inMemoryManga.insert('', [], newManga);
+    expect(result).toBeFalsy();
+  });
+
+  test('should update an manga', async () => {
+    const newManga: Manga = {
+      name: 'manga2',
+      photo: 'photo.webp',
+      synopsis: 'text',
+      comment: 'text',
+      info: {
+        author: 'author',
+        status: 'status',
+        numberChapters: 12,
+        numberVolumes: 1,
+      },
+      whereRead: [{ language: 'sda', name: 'name', url: 'url' }],
+    };
+    const result = await inMemoryManga.update('', 'manga2', newManga);
+    expect(result).toBeTruthy();
+  });
+  test('should not update an manga when not find', async () => {
+    const newManga: Manga = {
+      name: 'manga2w',
+      photo: 'photo.webp',
+      synopsis: 'text',
+      comment: 'text',
+      info: {
+        author: 'author',
+        status: 'status',
+        numberChapters: 12,
+        numberVolumes: 1,
+      },
+      whereRead: [{ language: 'sda', name: 'name', url: 'url' }],
+    };
+    const result = await inMemoryManga.update('', 'manga2w', newManga);
+    expect(result).toBeFalsy();
+  });
+  test('should not update an manga have different names', async () => {
+    const newManga: Manga = {
+      name: 'manga2',
+      photo: 'photo.webp',
+      synopsis: 'text',
+      comment: 'text',
+      info: {
+        author: 'author',
+        status: 'status',
+        numberChapters: 12,
+        numberVolumes: 1,
+      },
+      whereRead: [{ language: 'sda', name: 'name', url: 'url' }],
+    };
+    const result = await inMemoryManga.update('', 'manga2w', newManga);
+    expect(result).toBeFalsy();
+  });
+});
---
+++ src/adapter/manga/repository/InMemoryManga.ts
@@ -0,0 +1,74 @@
+import MangaRepository from '../../../usecase/port/AnimeMangaRepository';
+import Manga from '../../../domain/manga';
+
+export default class InMemoryMangaReposiory implements MangaRepository<Manga> {
+  mangas: Manga[] = [];
+  constructor(mangas: Manga[]) {
+    this.mangas = mangas;
+  }
+
+  async _delete(_: string, name: string): Promise<boolean> {
+    if (await this.alreadyExists(_, name)) {
+      this.mangas = this.mangas.filter((manga) => manga.name !== name);
+      return true;
+    }
+    return false;
+  }
+
+  async alreadyExists(_: string, name: string): Promise<boolean> {
+    for (let manga of this.mangas) if (manga.name === name) return true;
+    return false;
+  }
+
+  async getOne(_: string, name: string, __: string[]): Promise<Manga | null> {
+    const manga = this.mangas.filter((manga) => manga.name === name);
+    if (manga.length > 0) {
+      return manga[0];
+    }
+    return null;
+  }
+  async getRandom(
+    _: string,
+    limit: number,
+    __: string[]
+  ): Promise<Array<Manga>> {
+    const mangas: Manga[] = [];
+    const numbers: Array<number> = [];
+    let i = 0;
+    while (i < limit) {
+      const number = Math.floor(Math.random() * limit);
+      if (!numbers.includes(number)) {
+        numbers.push(number);
+        mangas.push(this.mangas[number]);
+        i++;
+      }
+      if (i === limit) break;
+    }
+    return mangas;
+  }
+
+  async insert(_: string, __: string[], data: Manga): Promise<boolean> {
+    if (await this.alreadyExists(_, data.name)) return false;
+    this.mangas.push(data);
+    return true;
+  }
+  async update(_: string, name: string, newData: Manga): Promise<boolean> {
+    if (
+      !(await this.alreadyExists(_, name)) ||
+      ((await this.alreadyExists(_, newData.name)) && newData.name !== name)
+    )
+      return false;
+    let index = 0;
+    for (let manga of this.mangas)
+      if (manga.name === name) index = this.mangas.indexOf(manga);
+    this.mangas[index].comment = newData.comment;
+    this.mangas[index].folder = newData.folder;
+    this.mangas[index].images = newData.images;
+    this.mangas[index].info = newData.info;
+    this.mangas[index].name = newData.name;
+    this.mangas[index].photo = newData.photo;
+    this.mangas[index].synopsis = newData.synopsis;
+    this.mangas[index].whereRead = newData.whereRead;
+    return true;
+  }
+}
